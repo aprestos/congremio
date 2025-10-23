b84a70f0b4b8b966879795efef26429cc145366f
Fábio Ferreira <fabiogasparferreira@gmail.com>
2025-10-23 11:55:15 +0100

    removed tailwindcss/vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueJsx(), vueDevTools()],
+  css: {
+    postcss: './postcss.config.cjs',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   build: {
-    // Use esbuild for faster builds
     minify: 'esbuild',
   },
 })
